476e025279d78acafbdef50790b61144551acb00
Ieuan Tudur Peace <ieuan@semantise.com>
2017-04-02 19:11:48 +0100

    Adding postgresql

--- Package.swift
+++ Package.swift
@@ -7,5 +7,9 @@ let package = Package(
             url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git",
             majorVersion: 2, minor: 0
         )
+        .Package(
+            url: "https://github.com/PerfectlySoft/Perfect-PostgreSQL.git",
+            majorVersion: 2, minor: 0
+        )
     ]
 )
