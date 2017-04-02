82eedfcd544fbf7fa63cff35a17188d47a1bce27
Ieuan Tudur Peace <ieuan@semantise.com>
2017-04-03 00:02:01 +0100

    Using the main from: Perfect-Turnstile-PostgreSQL-Demo

--- Sources/main.swift
+++ Sources/main.swift
@@ -2,24 +2,104 @@ import PerfectLib
 import PerfectHTTP
 import PerfectHTTPServer
 
+import StORM
+import PostgresStORM
+import PerfectTurnstilePostgreSQL
+import TurnstilePerfect
+
+// Used later in script for the Realm and how the user authenticates.
+let pturnstile = TurnstilePerfectRealm()
+
+PostgresConnector.host        = "localhost"
+PostgresConnector.username    = "perfect"
+PostgresConnector.password    = "perfect"
+PostgresConnector.database    = "perfect_testing"
+PostgresConnector.port        = 5432
+
+
+// Set up the Authentication table
+let auth = AuthAccount()
+try? auth.setup()
+
+// Connect the AccessTokenStore
+tokenStore = AccessTokenStore()
+try? tokenStore?.setup()
+
+//let facebook = Facebook(clientID: "CLIENT_ID", clientSecret: "CLIENT_SECRET")
+//let google = Google(clientID: "CLIENT_ID", clientSecret: "CLIENT_SECRET")
 // Create HTTP server.
 let server = HTTPServer()
 
-// Register your own routes and handlers
+// Register routes and handlers
+let authWebRoutes = makeWebAuthRoutes()
+let authJSONRoutes = makeJSONAuthRoutes("/api/v1")
+
+// Add the routes to the server.
+server.addRoutes(authWebRoutes)
+server.addRoutes(authJSONRoutes)
+
+// Adding a test route
 var routes = Routes()
-routes.add(method: .get, uri: "/", handler: {
+routes.add(method: .get, uri: "/api/v1/test", handler: AuthHandlersJSON.testHandler)
+
+// An example route where authentication will be enforced
+routes.add(method: .get, uri: "/api/v1/check", handler: {
     request, response in
-    response.setHeader(.contentType, value: "text/html")
-    response.appendBody(string: "<html><title>Hello, world!</title><body>Hello, world!</body></html>")
+    response.setHeader(.contentType, value: "application/json")
+    
+    var resp = [String: String]()
+    resp["authenticated"] = "AUTHED: \(request.user.authenticated)"
+    resp["authDetails"] = "DETAILS: \(request.user.authDetails)"
+    
+    do {
+        try response.setBody(json: resp)
+    } catch {
+        print(error)
+    }
     response.completed()
-}
-)
+})
+
+// An example route where auth will not be enforced
+routes.add(method: .get, uri: "/api/v1/nocheck", handler: {
+    request, response in
+    response.setHeader(.contentType, value: "application/json")
+    
+    var resp = [String: String]()
+    resp["authenticated"] = "AUTHED: \(request.user.authenticated)"
+    resp["authDetails"] = "DETAILS: \(request.user.authDetails)"
+    
+    do {
+        try response.setBody(json: resp)
+    } catch {
+        print(error)
+    }
+    response.completed()
+})
+
+
 
 // Add the routes to the server.
 server.addRoutes(routes)
 
+// add routes to be checked for auth
+var authenticationConfig = AuthenticationConfig()
+authenticationConfig.include("/api/v1/check")
+authenticationConfig.exclude("/api/v1/login")
+authenticationConfig.exclude("/api/v1/register")
+
+let authFilter = AuthFilter(authenticationConfig)
+
+// Note that order matters when the filters are of the same priority level
+server.setRequestFilters([pturnstile.requestFilter])
+server.setResponseFilters([pturnstile.responseFilter])
+
+server.setRequestFilters([(authFilter, .high)])
+
 // Set a listen port of 8181
 server.serverPort = 8181
+
+// Where to serve static files from
+server.documentRoot = "./webroot"
 
 do {
     // Launch the HTTP server.
